bba9b549a605168c0fcefcd1dc9057deef06dde3
Doug Thistlewolf <dthistlewolf@exploratorium.edu>
2024-08-19 11:01:42 -0700

    initial commit

---
+++ ChatGPT/main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron');
+const fs = require('fs');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // Load an external webpage
+  win.loadURL('https://chatgpt.com/');
+
+  // Once the webpage is fully loaded, inject the CSS
+  win.webContents.on('did-finish-load', () => {
+    const cssPath = path.join(__dirname, 'inject.css');
+    const css = fs.readFileSync(cssPath, 'utf-8');
+    win.webContents.insertCSS(css);
+  });
+
+ win.webContents.openDevTools(); //open dev tools
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
